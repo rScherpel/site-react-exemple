deeb0a9dadccdd3ee6cc7791ff39fc782f795d6c
Rafael <scherpelrafa@gmail.com>
2026-01-17 17:54:50 -0300

    feat: adding useDeviceType hook

---
+++ src/hooks/useDeviceType.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export function useDeviceType() {
+  const getType = () => {
+    const width = window.innerWidth;
+
+    if (width <= 576) return "celular";
+    if (width <= 992) return "tablet";
+    return "notebook";
+  };
+
+  const [device, setDevice] = useState(getType());
+
+  useEffect(() => {
+    const handleResize = () => setDevice(getType());
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return device;
+}
