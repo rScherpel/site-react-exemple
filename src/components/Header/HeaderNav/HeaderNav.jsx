611cb958142353fb19c6d729af1e1a8685e2ba59
Rafael <scherpelrafa@gmail.com>
2026-01-18 16:58:22 -0300

    reafactor: responsiveness for headerNav

--- src/components/Header/HeaderNav/HeaderNav.jsx
+++ src/components/Header/HeaderNav/HeaderNav.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import { useDeviceType } from "../../../hooks/useDeviceType";
 import "./HeaderNav.css";
 
-export default function Home() {
+export default function HeaderNav() {
+  const device = useDeviceType();
+
+  const navClass =
+    device === "celular" ? "header-nav-mobile" : "header-nav";
+
   return (
-    <nav className="header-nav">
+    <nav className={navClass}>
       <Link to="/">Home</Link>
       <Link to="/team">Nossa Equipe</Link>
       <Link to="/contact">Contatos</Link>
