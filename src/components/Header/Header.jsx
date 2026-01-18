908bf99d655a7a92a8a2f2ce50e368707a85a9a3
Rafael <scherpelrafa@gmail.com>
2026-01-18 16:58:00 -0300

    feat: reponsivess for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,51 @@
 import "./Header.css";
 import HeaderNav from "./HeaderNav/HeaderNav.jsx";
 import { SITE } from "../../config/config";
+import { useDeviceType } from "../../hooks/useDeviceType.jsx";
+import Accordion from "../Accordion/Accordion.jsx";
+import { IoIosMenu } from "react-icons/io";
 
 export default function Header() {
-  return (
-    <header className="header">
-      <div className="header-container">
-        <button className="header-logo-wrap">
+  const device = useDeviceType();
+
+  if (device !== "notebook") {
+    return (
+      <header className="header">
+        <a className="header-logo-wrap">
           <img
-            src=".\src\assets\images\logo com texto branca.jpg"
+            src="/src/assets/images/logo com texto branca.jpg"
             alt="imagem logo"
             className="header-logo"
           />
-        </button>
-
-        <HeaderNav/>
-
+        </a>
+        <div>
+          <Accordion
+            className="accordion"
+            wrapperClass="accordion-container"
+            buttonClass="accordion-button"
+            panelClass="accordion-panel"
+            items={[
+              {
+                title: <IoIosMenu size={27}/>,
+                content: <HeaderNav />
+              },
+            ]}
+          />
+        </div>
+      </header>
+    );
+  }
+  return (
+    <header className="header">
+      <div className="header-container">
+        <a className="header-logo-wrap" href="/home">
+          <img
+            src="/src/assets/images/logo com texto branca.jpg"
+            alt="imagem logo"
+            className="header-logo"
+          />
+        </a>
+        <HeaderNav />
         <a
           href={SITE.whatsapp_link}
           className="header-button"
