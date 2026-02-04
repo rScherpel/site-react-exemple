73151441a81cb519e342053ed9959fa0b34082e0
rScherpel <scherpelrafa@gmail.com>
2026-02-04 19:45:28 -0300

    fix: changing links

--- src/pages/pageComponents/HeaderNav/index.jsx
+++ src/pages/pageComponents/HeaderNav/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import useDeviceType from "../../../hooks/useDeviceType";
 import "./style.css";
+import Contacts from "../../Contacts";
 
 export default function HeaderNav() {
   const device = useDeviceType();
@@ -11,10 +12,16 @@ export default function HeaderNav() {
   return (
     <nav className={navClass}>
       <Link to="/">Home</Link>
-      <a href="https://github.com/rScherpel/site-react-exemple" target="_blank" rel="noopener noreferrer">GitHub</a>
-      <Link to="/techs">Tecnologias</Link>
+      <a
+        href="https://github.com/rScherpel/site-react-exemple"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Projetos
+      </a>
+      <Link to="/techs">Stack</Link>
+      <Link to="/contacts">Contatos</Link>
       <Link to="/about">Sobre</Link>
-      <Link to="/contact">Contatos</Link>
     </nav>
   );
 }
