7ef4efe42c46b18fc03d88488aceede13c69bd2e
rScherpel <scherpelrafa@gmail.com>
2026-02-04 19:46:35 -0300

    fix routes

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Contacts from "./Contacts";
 import PrivacyPolicy from "./PrivacyPolicy";
 
 export default function Body() {
-  return(    
+  return (
     <main>
-        <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      </Routes>
     </main>
-    )
+  );
 }
+  
