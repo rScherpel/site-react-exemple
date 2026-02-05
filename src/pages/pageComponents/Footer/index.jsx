7593fcace51a8f90eb6dae545e445bebbf7c60b0
rScherpel <scherpelrafa@gmail.com>
2026-02-04 23:37:16 -0300

    refactor: new footer design

--- src/pages/pageComponents/Footer/index.jsx
+++ src/pages/pageComponents/Footer/index.jsx
@@ -1,54 +1,17 @@
 import { Link } from "react-router-dom";
 import { SITE } from "src/config";
 import "./style.css";
-import ContactActions from "src/components/ContactActions/index.jsx";
+import ContactActions from "src/pages/pageComponents/ContactActions";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <ul className="footer-list">
-        <li className="foooter-logo-img">
-          <img
-            src=".\src\assets\images\logo sem fundo.svg"
-            alt=""
-            className="foooter-logo-img"
-          />
-        </li>
-        <li>
-          <h1 className="list-tittles" id="link-tittle">
-            Links
-          </h1>
-          <ul className="footer-sublist">
-            <li className="quick-links">
-              <Link to="/practice-areas">Áreas de Atuação</Link>
-            </li>
-            <li className="quick-links">
-              <Link to="/contact">Contatos</Link>
-            </li>
-            <li className="quick-links">
-              <Link to="/team">Nossa Equipe</Link>
-            </li>
-            <li className="quick-links">
-              <Link to="/our-office">Nosso escritório</Link>
-            </li>
-          </ul>
-        </li>
-
-        <li>
-          <h1 className="list-tittles" id="contact-li">
-            Entre em Contato
-          </h1>
-          <ul>
-            <ContactActions />
-          </ul>
-        </li>
-      </ul>
-
-      <div className="footer-bottom">
-        <p>© 2025 {SITE.name}. Todos os direitos reservados.</p>
-        <div className="footer-links">
-          <Link to="/privacy-policy">Privacy Policy</Link>
-        </div>
+      <div className="footer-content">
+        <ContactActions />
+        <p>
+          &copy; {new Date().getFullYear()} {SITE.owner}. Todos os direitos
+          reservados.
+        </p>
       </div>
     </footer>
   );
