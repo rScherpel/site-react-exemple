297d37f53409f3c9bc4d627239c7a960133c828e
rScherpel <scherpelrafa@gmail.com>
2026-02-05 01:26:28 -0300

    feat: new feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import Footer from "./pages/pageComponents/Footer";
 import Body from "./pages/Body";
 import "./App.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <ThemeProvider>
+      <Toaster position="top-center" />
+      
       <div className="app">
-      <Header />
-      <main>
-        <Body />
-      </main>
-      <Footer />
-    </div>
+        <Header />
+        <main>
+          <Body />
+        </main>
+        <Footer />
+      </div>
     </ThemeProvider>
   );
 }
--- src/pages/pageComponents/ThemeSwitch/index.jsx
+++ src/pages/pageComponents/ThemeSwitch/index.jsx
@@ -1,13 +1,31 @@
-import FormControlLabel from "@mui/material/FormControlLabel";
+import toast from "react-hot-toast";
+import { useTheme } from "../../../contexts/ThemeContext";
 import { ThemeSwitchStyled } from "./style";
-import { useTheme } from "src/contexts/ThemeContext.jsx";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 export default function ThemeSwitch() {
   const { dark, toggleTheme } = useTheme();
 
+  function handleToggle() {
+    if (dark) {
+      toast("☀️ Modo claro ativado. cuidado com os olhos!", {
+        style: {
+          background: "#0a192f",
+          color: "#00ffd1",
+        },
+      });
+    }
+    toggleTheme();
+  }
+
   return (
     <FormControlLabel
-      control={<ThemeSwitchStyled checked={dark} onChange={toggleTheme} />}
+      control={
+        <ThemeSwitchStyled
+          checked={dark}
+          onChange={handleToggle}
+        />
+      }
       label={dark ? "Dark" : "Light"}
       sx={{
         "& .MuiFormControlLabel-label": {
